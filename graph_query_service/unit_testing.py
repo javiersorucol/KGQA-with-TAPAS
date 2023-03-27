ebdb5a7ca2049b7dbfe9301c63b0d3c10444ef6f
javiersorucol <114678290+javiersorucol@users.noreply.github.com>
2023-03-27 10:41:18 -0400

    Update in graph query unit tests
    - get_value_by_type function tests implemented

--- graph_query_service/unit_testing.py
+++ graph_query_service/unit_testing.py
@@ -11,6 +11,8 @@ import unittest
 from utils.Request_utils import query_api
 from utils.Configuration_utils import read_config_file
 
+from graph_query_service.service  import get_value_by_type
+
 config = read_config_file('App_config.ini')
 
 graph_query_service = dict(config.items('GRAPH_QUERY_SERVICE'))
@@ -49,7 +51,69 @@ class Graph_Query_testing(unittest.TestCase):
         res = query_api('get', graph_query_url + endpoint + entity_UID, {}, {}, {})
         self.assertEqual(res.get('code'), 400 , '/entity/ endpoint is working with an unexisting entity UID.')
 
-    
+    def test_get_value_by_type_function(self):
+        # case quntity
+        res = get_value_by_type('quantity', {
+            "value": {
+                "amount": "+115",
+                "unit": "http://www.wikidata.org/entity/Q25250"
+            },
+            "type": "quantity"
+        })
+        self.assertEqual('+115', res, 'get_value_by_type funcion is failing in quantity case.')
+        # case url
+        res = get_value_by_type('url', {
+            "value": "http://www.iec.ch/worldplugs/list_bylocation.htm",
+            "type": "string"
+        })
+        self.assertEqual('http://www.iec.ch/worldplugs/list_bylocation.htm', res, 'get_value_by_type funcion is failing in url case.')
+        #case time
+        res = get_value_by_type('time', {
+            "value": {
+                "time": "+2016-06-10T00:00:00Z",
+                "timezone": 120,
+                "before": 0,
+                "after": 0,
+                "precision": 11,
+                "calendarmodel": "http://www.wikidata.org/entity/Q1985727"
+            },
+            "type": "time"
+        })
+        self.assertEqual('+2016-06-10T00:00:00Z', res, 'get_value_by_type funcion is failing in time case.')
+        # case wikibase item
+        res = get_value_by_type('wikibase-item', {
+            "value": {
+                "entity-type": "item",
+                "numeric-id": 193858,
+                "id": "Q193858"
+            },
+            "type": "wikibase-entityid"
+        })
+        self.assertEqual('Q193858', res, 'get_value_by_type funcion is failing in wikibase-item case.')
+        # case monolingualtext
+        res = get_value_by_type('monolingualtext', {
+            "value": {
+                "text": "World Plugs",
+                "language": "en"
+            },
+            "type": "monolingualtext"
+        })
+        self.assertEqual('World Plugs', res, 'get_value_by_type funcion is failing in monolingual case.')
+        # case global  coordinate
+        res = get_value_by_type('globe-coordinate', {
+            "value": {
+                "latitude": -9.67,
+                "longitude": -65.45,
+                "altitude": 1,
+                "precision": 1.0e-6,
+                "globe": "http://www.wikidata.org/entity/Q2"
+            },
+            "type": "globecoordinate"
+        })
+        print(res)
+        self.assertIn(str(-9.67), res, 'get_value_by_type funcion is failing in global-coordinate case, latitude not found.')
+        self.assertIn(str(-65.45), res, 'get_value_by_type funcion is failing in global-coordinate case, longitude not found.')
+        self.assertIn(str(1), res, 'get_value_by_type funcion is failing in global-coordinate case, altitude not found.')        
 
 if __name__ == '__main__':
     unittest.main()
