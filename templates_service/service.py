04ea27adbbfddb437717a63314211e916bf97364
javiersorucol <114678290+javiersorucol@users.noreply.github.com>
2023-04-10 17:17:05 -0400

    Templates service skel created

---
+++ templates_service/service.py
@@ -0,0 +1,20 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+from fastapi import FastAPI, HTTPException
+
+from utils.Configuration_utils import read_config_file
+
+# Reading the config file
+config_file_path = 'templates_service/Config/Config.ini'
+config = read_config_file(config_file_path)
+
+# saving config vars
+
+
+app = FastAPI()
+
+@app.post('/test/')
+def test():
+    return 'Success'
